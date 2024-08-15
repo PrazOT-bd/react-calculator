e7d8edfca495ea6e681fdca40e2d9fb52c1ea0bd
PrazOT <praiseotoro@gmail.com>
2024-08-15 23:42:07 +0100

    react calculator

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -13,9 +13,9 @@ function Keypad({ handleClick }) {
       <button onClick={() => handleClick('5')}>5</button>
       <button onClick={() => handleClick('6')}>6</button>
       <button className="operation" onClick={() => handleClick('-')}>-</button>
-      <button onClick={() => handleClick('1')}>7</button>
-      <button onClick={() => handleClick('2')}>8</button>
-      <button onClick={() => handleClick('3')}>9</button>
+      <button onClick={() => handleClick('7')}>7</button>
+      <button onClick={() => handleClick('8')}>8</button>
+      <button onClick={() => handleClick('9')}>9</button>
       <button className="operation" onClick={() => handleClick('*')}>*</button>
       <button className="clear" onClick={() => handleClick('Clear')}>C</button>
       <button onClick={() => handleClick('0')}>0</button>
